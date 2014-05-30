a29fd9cc33d6d995301a45b477d492aec2cb4597
Jeff Mitzelfelt <jdmitz@gmail.com>
2014-05-30 09:22:59 -0500

    Modified Styling & Refactor
    Set up full page CSS with Heading, Chart, Feature Info
    Refactored map.js to closure

--- app/js/map.js
+++ app/js/map.js
@@ -1,45 +1,46 @@
 
-var maxMapBounds = L.latLngBounds(L.latLng(36.9, -91.6),L.latLng(42.6, -87.4));
+var permitMap = function(){
+  var maxMapBounds = L.latLngBounds(L.latLng(36.9, -91.6),L.latLng(42.6, -87.4));
 
-var map = {};
-var baseMap = L.esri.basemapLayer("Topographic");
+  var map = {};
+  var baseMap = L.esri.basemapLayer("Topographic");
 
-var baseMapOverlay = L.esri.dynamicMapLayer('http://geoservices.epa.illinois.gov/ArcGIS/rest/services/SWAP/Location/MapServer',{
-  opacity: 0.75,
-  layers: [10, 12]
-});
+  var baseMapOverlay = L.esri.dynamicMapLayer('http://geoservices.epa.illinois.gov/ArcGIS/rest/services/SWAP/Location/MapServer',{
+    opacity: 0.75,
+    layers: [10, 12]
+  });
 
-function colorCountyFeature(feature){
-      var colorVal = '#F00';
-      if (feature.properties.TotalBOA_BOW < 50){
-        colorVal = '#0FF';
-      }
-      else if (feature.properties.TotalBOA_BOW < 100){
-        colorVal = '#0F0';
-      }
-      else if (feature.properties.TotalBOA_BOW < 300){
-        colorVal = '#FF0';
-      }
-      else if (feature.properties.TotalBOA_BOW < 1000){
-        colorVal = '#F60';
-      }
-      return {color: colorVal};
+  function colorCountyFeature(feature){
+    var colorVal = '#F00';
+    if (feature.properties.TotalBOA_BOW < 50){
+      colorVal = '#0FF';
     }
+    else if (feature.properties.TotalBOA_BOW < 100){
+      colorVal = '#0F0';
+    }
+    else if (feature.properties.TotalBOA_BOW < 300){
+      colorVal = '#FF0';
+    }
+    else if (feature.properties.TotalBOA_BOW < 1000){
+      colorVal = '#F60';
+    }
+    return {color: colorVal};
+  }
 
-function configureCountyFeature(feature, layer) {
-      layer.bindPopup('<h3>'+feature.properties.NAME_LC+'</h3>');
-      layer.on('mouseover mousemove', function(e){
-        var hover_bubble = new L.Rrose({
-          offset: new L.Point(0,-10),
-          closeButton: false,
-          autoPan: false}
-          ).setContent('<p>'+feature.properties.NAME_LC+'<br>'+feature.properties.TotalBOA_BOW + ' Permits</p>')
-        .setLatLng(e.latlng)
-        .openOn(map);
-      });
-      layer.on('mouseout', function(e){
-        map.closePopup();
-      });
+  function configureCountyFeature(feature, layer) {
+    layer.bindPopup('<h3>'+feature.properties.NAME_LC+'</h3>');
+    layer.on('mouseover mousemove', function(e){
+      var hover_bubble = new L.Rrose({
+        offset: new L.Point(0,-10),
+        closeButton: false,
+        autoPan: false}
+        ).setContent('<p>'+feature.properties.NAME_LC+'<br>'+feature.properties.TotalBOA_BOW + ' Permits</p>')
+      .setLatLng(e.latlng)
+      .openOn(map);
+    });
+    layer.on('mouseout', function(e){
+      map.closePopup();
+    });
       // layer.on('mousedown', function(e){
       //   //layer.off('mouseover mousemove');
       //   var popup = new L.Popup({
@@ -50,7 +51,7 @@ function configureCountyFeature(feature, layer) {
       //   .setLatLng(e.latlng)
       //   .openOn(map);
       // });
-    }
+}
 
 var featureLayers = new L.esri.FeatureLayer("http://epa084dgis01.iltest.illinois.gov:6080/arcgis/rest/services/TPservices/PermitTotalsCounty_WM/FeatureServer/0", {
   style: colorCountyFeature,
@@ -64,4 +65,8 @@ map = L.map('divMap',{
   maxBounds: maxMapBounds
 }).setView([40, -89.5],7).addLayer(featureLayers).addLayer(baseMapOverlay, true);
 
-baseMapOverlay.bringToBack();
+baseMapOverlay.bringToBack();  
+}();
+
+
+
